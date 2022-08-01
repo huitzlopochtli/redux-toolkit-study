8183ceff812c8146dd4b4ce7e07ba07edafe740c
peeyalk <khondokar.hash@gmail.com>
2022-08-01 19:53:26 +0900

    Add Error Page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact/Contact";
 import Counter from "./components/Counter/Counter";
+import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
 
 export default function AppRoutes() {
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/counter" element={<Counter />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
